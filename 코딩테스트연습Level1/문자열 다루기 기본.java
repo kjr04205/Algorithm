f3b1e5eda7c7d7ff5bac72fe20016dfd9c225a2d
kjr04205 <kjr04205@nate.com>
2022-01-27 16:05:33 +0900

    study

---
+++ 코딩테스트연습Level1/문자열 다루기 기본.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if(s.length() < 4 || s.length() > 6 || s.length() == 5){
+            answer = false;
+        }
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) < '0' || s.charAt(i) > '9'){
+                answer = false;
+            }            
+        }
+
+        return answer;
+    }
+}
