fa063e386c2194cb3d62cb6a7465d4e827b0bd43
kjr04205 <kjr04205@nate.com>
2022-01-27 11:23:51 +0900

    code test study

---
+++ 코딩테스트연습Level1/정수제곱근판별.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        for(long i=0; i<=n; i++){
+            if(i*i == n){
+                answer = (i+1)*(i+1);
+                break;
+            }else{
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
