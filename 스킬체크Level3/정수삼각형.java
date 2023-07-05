6b492716cf9d062cef356daa4946bb857e8bdf68
hyoin <kjr04205@nate.com>
2023-07-05 18:57:39 +0900

    스킬체크 Level3 합격!

---
+++ 스킬체크Level3/정수삼각형.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < triangle.length; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+            }
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        }
+        int answer = 0;
+        for (int i = 0; i < triangle.length; i++) {
+            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        }
+        return answer;
+    }
+}
